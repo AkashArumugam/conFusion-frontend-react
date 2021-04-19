5081affe63c77cabeaa37e3f15d038428d9f2024
AkashArumugam <44131956+AkashArumugam@users.noreply.github.com>
2021-04-19 13:34:29 +0530

    Presentational and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-
-    constructor(props) {
-        super(props);
-    }
 
     renderDish(dish) {
         if (dish != null)
@@ -30,7 +26,11 @@ class DishDetail extends Component {
         if(this.props.dish) {
             comments = this.props.dish.comments.map((comment) => {
                 return (
-                    <li>{comment.comment}</li>
+                    <li>
+                        {comment.comment}
+                        <br />---------&gt;
+                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}    
+                    </li>
                 );
             });
 
@@ -50,12 +50,14 @@ class DishDetail extends Component {
         
 
         return (
-            <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                {this.renderDish(this.props.dish)}
-                </div>
-                <div  className="col-12 col-md-5 m-1">
-                    {this.renderComments()}
+            <div className="container">
+                <div className="row">
+                    <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                    </div>
+                    <div  className="col-12 col-md-5 m-1">
+                        {this.renderComments()}
+                    </div>
                 </div>
             </div>
         );
